2f6bdda82e57c653629caa350893fb3fbffeacf1
erichengyc <68725887+erichengyc@users.noreply.github.com>
2021-09-23 07:56:43 +0930

    move all the folders and files

---
+++ sw.js
@@ -0,0 +1,55 @@
+const staticCache = 'static-cache';
+const dynamicCache = 'dynamic-cache';
+
+const assets = [
+    '/',
+    '/index.html', 
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/materialize.min.css',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    '/pages/fallback.html'
+
+]
+
+const limitNumCache = (cacheName, num)=>{
+    caches.open(cacheName).then(cache => {
+        cache.keys().then(keys =>{
+            if (keys.length > num) {
+                cache.delete(keys[0]).then(limitNumCache
+                (cacheName, num))
+            }
+        })
+    })
+}
+
+
+
+// install process
+self.addEventListener('install', e => {
+    e.wa
+    caches.open(staticCache).then(cache => {
+        cache.addAll(assets)
+    })
+})
+
+// activate 
+self.addEventListener('activate', e => {
+    console.log('sw is activated')
+})
+
+self.addEventListener('fetch', e=> {
+    e.respondWith(
+        caches.match(e.request).then(staticRes=>{
+            return staticRes || fetch(e.request).then(dynamicRes=>{
+                return caches.open(dynamicCache).then(cache=>{
+                    cache.put(e.request.url, dynamicRes.clone())
+                    limitNumCache(dynamicCache, 2)
+                    return dynamicRes
+                })
+            })
+        }).catch(()=>caches.match('/pages/fallback.html'))
+    )
+})
+
